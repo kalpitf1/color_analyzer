8d350df25b1a7f6a5eb1b841b0f51cbfdf903e7d
kalpitfulwariya <kalpit.fulwariya@kaleidoscope-blockchain.com>
2024-09-27 14:43:26 +0530

    feat: change title; add drag and drop + upload to receive file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
-import { useState } from 'react'
+import { React } from 'react'
 import { Upload } from "lucide-react"
 import { SiGithub as Github } from '@icons-pack/react-simple-icons'
+import Dropzone from 'react-dropzone'
 import './App.css'
 
 function App() {
+
   return (
     <div className='flex flex-col h-screen'>
       <header className="flex items-center justify-between px-6 py-4 bg-stone-100 border-b">
@@ -18,20 +20,29 @@ function App() {
         </nav>
       </header>
 
-      <main className="flex-grow flex flex-col items-center justify-center p-6 bg-gradient-to-br from-fuchsia-100 to-green-100">
+      <main className="flex-grow flex flex-col items-center p-6 bg-gradient-to-br from-fuchsia-100 to-green-100">
         <h2 className="text-4xl font-bold text-center mb-4">Start Korean Color Analysis</h2>
         <p className="text-xl text-gray-600 text-center mb-8">
           100% automatic and free
         </p>
         <div className="w-full max-w-xl bg-white rounded-lg shadow-lg p-8">
-          <div className="border-2 border-dashed border-gray-300 rounded-lg p-12 text-center">
-            <Upload className="mx-auto h-12 w-12 text-gray-400" />
-            <p className="mt-4 text-lg text-gray-600">Drag and drop your image here</p>
-            <p className="mt-2 text-sm text-gray-500">or</p>
-            <button className="mt-4 px-6 py-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition duration-300">
-              Upload Image
-            </button>
-          </div>
+          <Dropzone onDrop={acceptedFiles => console.log(acceptedFiles)}>
+            {({getRootProps, getInputProps}) => (
+              <section>
+                <div {...getRootProps()}>
+                  <input {...getInputProps()} />
+                  <div className='flex-grow flex flex-col items-center justify-center'>
+                    <Upload className="mx-auto h-12 w-12 text-gray-400" />
+                    <p className="mt-4 text-lg text-gray-600">Drag and drop your image here</p>
+                    <p className="mt-2 text-sm text-gray-500">or</p>
+                    <button className="mt-4 px-6 py-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition duration-300">
+                      Upload Image
+                    </button>
+                  </div>
+                </div>
+              </section>
+            )}
+          </Dropzone>
         </div>
       </main>
 
